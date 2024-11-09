3646f81502d03c77a8573322b1e767ee37466b64
Roman2423 <void.knight2007@gmail.com>
2024-11-09 11:12:56 +0200

    death screen

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ pg.init()
 pg.font.init()
 
 # Звуки #
+maps = ("levels/level1.txt")
 background_music = pg.mixer.music.load("files/background_music.mp3")
 pg.mixer.music.set_volume(0.5)
 pg.mixer.music.play(-1)
@@ -19,13 +20,14 @@ clock = pg.time.Clock()
 
 
 # Сам Цикл #
-def game_loop():  
-    walls, player, enemies = load_map("map.txt")  # Загрузка карты #
+def game_loop(): 
+    walls, player, enemies = load_map("levels/level1.txt")  # Загрузка карты #
 
     bullets = pg.sprite.Group()
     explosions = pg.sprite.Group()
 
     running = True
+    i = 0
     while running:
         for event in pg.event.get():
             if event.type == pg.QUIT:
@@ -36,7 +38,7 @@ def game_loop():
 
                 if event.key == pg.K_ESCAPE:
                     pause_menu(screen)
-                    
+
         player.update(walls)
         for enemy in enemies:
             enemy.update(player, walls)
@@ -58,7 +60,10 @@ def game_loop():
                         explosions.add(explosion)
                         bullet.kill()
                         enemy.kill()
-
+                        conditional = True
+                        if conditional:
+                            i += 1
+                            walls, player, enemy = load_map(maps[i])
         if player.alive:
             for bullet in bullets:
                 if bullet.rect.colliderect(player.rect):
